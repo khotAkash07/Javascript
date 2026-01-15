3b9ec394caeaa55bff73a2c8a585532e70a97752
khotAkash07 <khotakash5737@gmail.com>
2026-01-15 18:52:29 +0530

    Array From method

--- js/Arrays/Reduce.js
+++ js/Arrays/Reduce.js
@@ -7,3 +7,8 @@ console.log(arr.filter((a)=> !(a % 2 === 0)))
 
 let arr2 = [[1,2,3],[4,5,6],[7,8,9]];
 console.log(arr2.flatMap(a => a));
+
+let str = "Akash Khot"
+
+let strArr = Array.from(str);
+console.log(strArr);
